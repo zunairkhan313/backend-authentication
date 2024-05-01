29529fb4b78f7190ea776388561c2b4b2533a69f
zunairkhan313 <zunairkhan742@gmail.com>
2024-05-01 13:06:12 -0700

    commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+}
